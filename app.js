9435ae321e1bb72bb377b3eae21d9a7ba30b95ad
Jony <jonathan_rivarola_91@hotmail.com>
2020-11-16 19:56:56 -0300

    trabajo terminado

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require('http');
+//console.log(http);
+
+http.createServer(function(req, res){
+
+    res.writeHead(200,{
+        "Content-Type" : "text/plain; charset=utf-8"
+    });
+
+    switch (req.url) {
+        case '/':
+            res.end('Este es el HOME')
+            break;
+        case '/productos' :
+            res.end('Estos son nuestros productos')
+            break;
+        case '/contacto' :
+            res.end('Dejanos tu mensaje')
+            break;    
+        default:
+            res.end('Pagina no disponible, vuelva mas tarde')
+            break;
+    }
+
+
+
+}).listen(3030,'localhost',()=>console.log('servidor funcionando en el puerto 3030'));
